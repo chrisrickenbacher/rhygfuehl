9695f9ead29102c90b666de5b2a2d7006c200d8d
Christian Rickenbacher <christian.rickenbacher@gmail.com>
2022-07-14 20:33:59 +0200

    new service worker definition for better cache handling of pwa

---
+++ src/sw.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+
+//Only if you use google analytics and wants to send the offline views
+workbox.googleAnalytics.initialize()
+
+//This is how you can use the network first strategy for files ending with .js
+workbox.routing.registerRoute(
+  /.*\.js/,
+  workbox.strategies.networkFirst()
+)
+
+// Use cache but update cache files in the background ASAP
+workbox.routing.registerRoute(
+  /.*\.css/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache'
+  })
+)
+
+//Cache first, but defining duration and maximum files
+workbox.routing.registerRoute(
+  /.*\.(?:png|jpg|jpeg|svg|gif)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
