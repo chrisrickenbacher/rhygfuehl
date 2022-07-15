9c3da6f4cb961e760b79d5bb9b3b3265cdb5f675
Christian Rickenbacher <christian.rickenbacher@gmail.com>
2022-07-15 08:47:50 +0200

    update serviceworker cache strategy and reload on update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -6,15 +6,14 @@ if (process.env.NODE_ENV === 'production') {
  register(`${process.env.BASE_URL}sw.js`, {
    ready () {
      console.log(
-       'App is being served from cache by a service worker.\n' +
-       'For more details, visit https://goo.gl/AFskqB'
+       'App is being served from cache by a service worker.'
      )
    },
    cached () {
      console.log('Content has been cached for offline use.')
    },
    updated () {
-     console.log('New content is available; please refresh.')
+    window.location.reload(true)
    },
    offline () {
      console.log('No internet connection found. App is running in offline mode.')
