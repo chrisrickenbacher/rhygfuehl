2b847e899876f027a89fc64e436362157782e78d
Christian Rickenbacher <christian.rickenbacher@gmail.com>
2022-07-17 11:06:28 +0200

    optimize data fresh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="p-2 mt-5 md:max-w-screen-md md:mx-auto">
-    <div v-if="refresh" class="text-center md:mb-2">
+  <div class="mt-5 md:max-w-screen-md md:mx-auto">
+    <div v-if="refresh" class="text-center fixed bottom-0 w-full md:static mb-5 md:mb-2">
       <button @click="refreshNow" type="button" class="inline-flex items-center px-3.5 py-2 border border-transparent text-md leading-4 font-medium rounded-full shadow-sm text-white bg-darkblue hover:bg-darkblue">
-        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
-        Refresh now
+        Refresh data
       </button>
     </div>
-    <main class="grid grid-cols-1	md:grid-cols-2">
+    <main class="p-2 grid grid-cols-1	md:grid-cols-2">
       <section v-for="(d, dIdx) in data" :key="dIdx" class="mb-5">
         <div class="text-2xl font-normal text-darkblue dark:text-white p-2"><h2>{{$t(d.name)}}</h2></div>
           <transition
