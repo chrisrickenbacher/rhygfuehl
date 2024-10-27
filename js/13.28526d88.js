ed5ee7e7589187456fe6b3f207380951e6d3cfa6
chrisrickenbacher <chrisrickenbacher@users.noreply.github.com>
2024-10-27 04:42:38 +0000

    deploy: 440d8c90d492bdfc692870fae91a13dc2c00b4aa

--- js/13.28526d88.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkrhygfuehl"]=self["webpackChunkrhygfuehl"]||[]).push([[13],{5013:function(e){e.exports=JSON.parse('{"actualValue":17.95,"lastUpdate":"2022-06-01T18:04:59+00:00","chart":{"week":[],"month":[]}}')}}]);
